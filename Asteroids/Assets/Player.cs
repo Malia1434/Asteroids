74f5e44f298e8120a102263fd8b5483c83d10dd1
Malia1434 <123518911+Malia1434@users.noreply.github.com>
2025-04-07 13:47:23 -0400

    Added in Player Inputs
    WASD, having issues deleting initial no binding in playerinputactions but the keys all seem to be working as expected.

--- Asteroids/Assets/Player.cs
+++ Asteroids/Assets/Player.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    //Player Input
+    private Vector2 input_vec = Vector2.zero;
+
+    public void CaptureMoveInput (InputAction.CallbackContext context)
+    {
+        input_vec = context.ReadValue<Vector2>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(input_vec);
     }
 }
